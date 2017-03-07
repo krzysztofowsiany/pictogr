00a69d492c49fe8cd494930eb38afd06e124fb09
Krzysztof Owsiany <k.owsiany@hydro-partner.pl>
2017-03-07 13:48:44 +0100

    Change MainWindow to SplashScreenView, use MVVM

--- PictOgr/App.xaml.cs
+++ PictOgr/App.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using PictOgr.SplashScreen.Views;
 
 namespace PictOgr
 {
@@ -13,5 +8,9 @@ namespace PictOgr
     /// </summary>
     public partial class App : Application
     {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            new SplashScreenView().Show();
+        }
     }
 }
---
+++ PictOgr/SplashScreen/ViewModels/SplashScreenViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using PictOgr.Annotations;
+
+namespace PictOgr.SplashScreen.ViewModels
+{
+    public class SplashScreenViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string property_name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property_name));
+        }
+    }
+}
