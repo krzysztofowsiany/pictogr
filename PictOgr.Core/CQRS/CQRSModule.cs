27d54defb5722500905ccca3ab340b3734ec5d63
Krzysztof Owsiany <gocom@gemustudio.com>
2017-03-10 14:43:31 +0100

    Use AutoFact to load base of CQRS in module

---
+++ PictOgr.Core/CQRS/CQRSModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using PictOgr.Core.CQRS.Command;
+using PictOgr.Core.CQRS.Query;
+
+namespace PictOgr.Core.CQRS
+{
+    public class CQRSModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register(c => new CommandBus(c)).As<ICommandBus>().SingleInstance();
+            builder.Register(c => new QueryBus(c)).As<IQueryBus>().SingleInstance();
+        }
+    }
+}
