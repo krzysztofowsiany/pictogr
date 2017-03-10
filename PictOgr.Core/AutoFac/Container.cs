124abc98c7210424de88398f1fd345f0952ed4d0
Krzysztof Owsiany <k.owsiany@hydro-partner.pl>
2017-03-10 13:41:31 +0100

    Add AutoFac container base implementation

---
+++ PictOgr.Core/AutoFac/Container.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Autofac;
+using Autofac.Core;
+
+namespace PictOgr.Core.AutoFac
+{
+    public class Container
+    {
+
+        private static ContainerBuilder CreateBuilder()
+        {
+            var builder = new ContainerBuilder();
+
+            LoadDLLs();
+
+            LoadModules(builder);
+
+            return builder;
+        }
+
+        private static void LoadDLLs()
+        {
+            foreach (var filePath in Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll"))
+            {
+                AppDomain.CurrentDomain.Load(Path.GetFileNameWithoutExtension(filePath));
+            }
+        }
+
+        private static void LoadModules(ContainerBuilder builder)
+        {
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(x => x.GetTypes())
+                .Where(t => t.IsAssignableTo<IModule>() && t.IsClass && !t.IsAbstract);
+
+            foreach (var type in types)
+            {
+                builder.RegisterModule((IModule) Activator.CreateInstance(type));
+            }
+        }
+    }
+}
