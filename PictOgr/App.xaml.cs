72659a7436a269ce3c1f6597fd44e22bb3bceb43
Krzysztof Owsiany <k.owsiany@hydro-partner.pl>
2017-03-24 13:44:24 +0100

    Add analuzer for code in all projects. Some fixes

--- PictOgr/App.xaml.cs
+++ PictOgr/App.xaml.cs
@@ -5,9 +5,6 @@ using PictOgr.SplashScreen.Views;
 
 namespace PictOgr
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
 	public partial class App : Application
 	{
 		private void OnStartup(object sender, StartupEventArgs e)
