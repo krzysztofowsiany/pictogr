b340ccbb2742c1beb16c8b39f00db8d928f06316
Krzysztof Owsiany <k.owsiany@hydro-partner.pl>
2017-03-29 07:57:12 +0200

    Write first command test

--- PictOgr.Core/Commands/ExitApplication.cs
+++ PictOgr.Core/Commands/ExitApplication.cs
@@ -4,11 +4,11 @@ namespace PictOgr.Core.Commands
 {
     public class ExitApplication : ICommand
     {
+        public int ExitCode { get; private set; }
+
         public ExitApplication(int exitCode)
         {
             ExitCode = exitCode;
         }
-
-        public int ExitCode { get; private set; }
     }
 }
---
+++ PictOgr.Tests/Core/CQRS/Commands/ExitApplicationCommandTest.cs
@@ -0,0 +1,26 @@
+﻿namespace PictOgr.Tests.Core.CQRS.Commands
+{
+    using PictOgr.Core.Commands;
+    using Shouldly;
+    using Xunit;
+
+    public class ExitApplicationCommandTest : CommandBaseTests<ExitApplication>
+    {
+        private readonly int expectedValue = 123;
+        private int exitCode;
+
+        [Fact]
+        public void exit_application_command_should_be_handled_by_command_bus()
+        {
+            handleMethod = command =>
+            {
+                var exitApplication = command as ExitApplication;
+                exitCode = exitApplication.ExitCode;
+            };
+
+            commandBus.SendCommand<ExitApplication>(new ExitApplication(expectedValue));
+
+            exitCode.ShouldBe(expectedValue);
+        }
+    }
+}
