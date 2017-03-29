978f02299e242ae17e1e60ce0f9921854b375492
Krzysztof Owsiany <gocom@gemustudio.com>
2017-03-29 09:53:45 +0200

    Add view command test for exit application.

--- PictOgr.Tests/Core/CQRS/Commands/ExitApplicationCommandTest.cs
+++ PictOgr.Tests/Core/CQRS/Commands/ExitApplicationCommandTest.cs
@@ -1,6 +1,7 @@
 ﻿namespace PictOgr.Tests.Core.CQRS.Commands
 {
     using PictOgr.Core.Commands;
+    using SplashScreen.Commands;
     using Shouldly;
     using Xunit;
 
@@ -9,18 +10,31 @@
         private readonly int expectedValue = 123;
         private int exitCode;
 
-        [Fact]
-        public void exit_application_command_should_be_handled_by_command_bus()
+        public ExitApplicationCommandTest()
         {
             handleMethod = command =>
             {
                 var exitApplication = command as ExitApplication;
                 exitCode = exitApplication.ExitCode;
             };
+        }
 
+        [Fact]
+        public void exit_application_command_should_be_handled_by_command_bus()
+        {
             commandBus.SendCommand<ExitApplication>(new ExitApplication(expectedValue));
 
             exitCode.ShouldBe(expectedValue);
+        }
+
+        [Fact]
+        public void window_command_exit_application_should_be_handle_by_command_bus()
+        {
+            var exitApplicationCommand = new ExitApplicationCommand(commandBus);
+
+            exitApplicationCommand.Execute(null);
+
+            exitCode.ShouldBe(0);
         }
     }
 }
