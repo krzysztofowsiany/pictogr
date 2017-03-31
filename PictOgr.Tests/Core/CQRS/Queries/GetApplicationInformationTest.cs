f8491bf7684b88ee09d229698191fb7c58ddba6c
Krzysztof Owsiany <gocom@gemustudio.com>
2017-03-31 15:04:01 +0200

    Add testing for Query resolve #1

---
+++ PictOgr.Tests/Core/CQRS/Queries/GetApplicationInformationTest.cs
@@ -0,0 +1,29 @@
+﻿namespace PictOgr.Tests.Core.CQRS.Queries
+{
+    using PictOgr.Core.Models;
+    using PictOgr.Core.Queries;
+    using Ploeh.AutoFixture;
+    using Shouldly;
+    using Xunit;
+
+    public class GetApplicationInformationTest : QueryBaseTests<GetApplicationInformation, ApplicationInformation>
+    {
+        private readonly Fixture fixture;
+
+        public GetApplicationInformationTest()
+        {
+            fixture = new Fixture();
+        }
+
+        [Fact]
+        public void get_application_version_should_return_the_random_string()
+        {
+            var version = fixture.Create<string>();
+            handleMethod = () => new ApplicationInformation(version);
+
+            var applicationInformation = queryBus.Process<GetApplicationInformation, ApplicationInformation>(new GetApplicationInformation());
+
+            applicationInformation.Version.ShouldBe(version);
+        }
+    }
+}
---
+++ PictOgr.Tests/Core/CQRS/Queries/QueryBaseTests.cs
@@ -0,0 +1,34 @@
+﻿namespace PictOgr.Tests.Core.CQRS.Queries
+{
+    using System;
+
+    using Autofac;
+    using FakeItEasy;
+    using FakeItEasy.Configuration;
+
+    using PictOgr.Core.AutoFac;
+    using PictOgr.Core.CQRS.Bus;
+    using PictOgr.Core.CQRS.Query;
+
+    public class QueryBaseTests<TQuery, TResult> where TQuery : IQuery<TResult>
+    {
+        protected IQueryBus queryBus;
+
+        protected Func<TResult> handleMethod;
+
+        public QueryBaseTests()
+        {
+            var builder = Container.CreateBuilder();
+
+            var fakeHandler = A.Fake<IQueryHandler<TQuery, TResult>>();
+
+            A.CallTo(() => fakeHandler.Execute(A<TQuery>._)).ReturnsLazily(() => handleMethod.Invoke());
+
+            builder.Register(c => fakeHandler).AsImplementedInterfaces();
+
+            var container = builder.Build();
+
+            queryBus = container.Resolve<IQueryBus>();
+        }
+    }
+}
