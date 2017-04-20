33f61a975950d2410c565024448291bdecdef2a1
Krzysztof Owsiany <gocom@o2.pl>
2017-04-20 20:42:27 +0200

    Move the Views to MVVM project. Add project for test End To End -> E2E.

--- PictOgr/Properties/Settings.Designer.cs
+++ PictOgr/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PictOgr.GUI.Properties {
+namespace PictOgr.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
